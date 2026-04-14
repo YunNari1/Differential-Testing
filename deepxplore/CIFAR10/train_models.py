d9b2587cf3e2f17db0b16fded23b892a21700386
YunNari <ynr2472@naver.com>
2026-04-14 21:32:40 +0900

    코드 정리

--- deepxplore/CIFAR10/train_models.py
+++ deepxplore/CIFAR10/train_models.py
@@ -15,7 +15,7 @@ y_test = to_categorical(y_test, 10)
 model1 = build_resnet50_model()
 model2 = build_resnet50_model()
 
-# 3. 서로 다르게 학습 (중요!)
+# 3. 서로 다르게 학습 
 model1.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 model2.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['accuracy'])
 
@@ -26,7 +26,7 @@ model1.fit(x_train, y_train, epochs=3, batch_size=64)
 print("Training model 2...")
 model2.fit(x_train, y_train, epochs=5, batch_size=64)
 
-# 5. 저장 (🔥 필수)
+# 5. 저장 
 model1.save("model1.h5")
 model2.save("model2.h5")
 
